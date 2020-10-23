51bb3a74edb92700faca9666005c02c545fe0da5
zenmpi <skypework1234@gmail.com>
2020-10-23 17:44:51 +0300

    Add file

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import telebot
-
+from telebot import types
 API_TOKEN = '496075695:AAGeItxJwnRi-pTgP5JWiIj08uFkbvsCPVk'
-
 bot = telebot.TeleBot(API_TOKEN)
 
 class ImportGoogleContactApp:
-    def __init__(self):
-        super(ImportGoogleContactApp, self).__init__()
-        self.launch_bot()
-    
     @bot.message_handler(commands=['start'])
-    def launch_bot(self):
+    def launch_bot(message):
         bot.send_message(message.chat.id, 'Bot start')
+
+    def load_keyboard(self, message):
+        keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*)
+if __name__ == '__main__':
+    app = ImportGoogleContactApp()
+    bot.polling(none_stop=True,timeout=123)
